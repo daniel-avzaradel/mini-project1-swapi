49220d4ac98003a222fe74c41f15dca5a9b92451
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-05-07 04:11:49 +0300

    mini-project1-swapi

---
+++ main.js
@@ -0,0 +1,45 @@
+const find = document.getElementById('find')
+const loader = document.querySelector('.load-wrapper')
+const notFound = document.querySelector('.not-found')
+
+const fullName = document.getElementById('name')
+const height = document.getElementById('height')
+const gender = document.getElementById('gender')
+const birthYear = document.getElementById('birthYear')
+const homeWorld = document.getElementById('homeWorld')
+
+let id = 0;
+
+const findSomeone = async () => {
+    id = Math.floor(Math.random() * 83);
+
+    fetch(`https://swapi.dev/api/people/${id}/`)
+    .then(loader.classList.remove('hidden'))
+    .then(response => response.json())
+    .then(setTimeout(() => {
+            loader.classList.add('hidden')
+    }, 500))
+    .then(data=> {
+        notFound.classList.add('hidden')
+        console.log(data)
+        fullName.textContent = data.name;
+        height.textContent = data.height;
+        gender.textContent = data.gender;
+        birthYear.textContent = data.birth_year;
+
+        fetch(`${data.homeworld}`)
+        .then(resp => resp.json())
+        .then(planet => {
+            homeWorld.textContent = planet.name
+        })
+        .catch(err => {
+            setTimeout(() => {
+            
+                notFound.classList.remove('hidden')
+        }, 500)
+        })
+    })
+    .catch(e => console.log(e, "failed"))
+}
+
+find.addEventListener('click', findSomeone)
